7541e8f8df7cb88bb9798c71a14ddba9c7c05eee
Akash <gnspda@gmail.com>
2024-06-05 15:32:16 +0530

    a

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ def test():
 
 @app.route("/", methods = ["GET","POST"])
 def home():
+    return render_template("helloworld.html")
+
+@app.route("/kml", methods = ["GET","POST"])
+def home():
     process_airtable_and_kml()
     print(200)
     return render_template("helloworld.html")
-
-if __name__ == '__main__':
-    app.run(debug=True)
