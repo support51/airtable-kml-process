54535458f827ab00e7f3e1fe2432d7d75d9e3425
Akash <gnspda@gmail.com>
2024-06-05 17:03:37 +0530

    sdf

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template("helloworld.html")
 
-@app.route("/kml", methods = ["GET","POST"])
-def home():
-    process_airtable_and_kml()
-    print(200)
-    return render_template("helloworld.html")
+# @app.route("/kml", methods = ["GET","POST"])
+# def home():
+#     process_airtable_and_kml()
+#     print(200)
+#     return render_template("helloworld.html")
 
 
 if __name__ == '__main__':
