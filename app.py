07c7444a9bb55e3abae4325bee447e50cffbee23
Akash <gnspda@gmail.com>
2024-06-05 16:36:31 +0530

    sdf

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
-# from airtableKmlProcess import process_airtable_and_kml
+from airtableKmlProcess import process_airtable_and_kml
 app = Flask(__name__)
+
 
 @app.route("/", methods = ["GET","POST"])
 def home():
     return render_template("helloworld.html")
 
-# @app.route("/kml", methods = ["GET","POST"])
-# def home():
-#     process_airtable_and_kml()
-#     print(200)
-#     return render_template("helloworld.html")
+@app.route("/kml", methods = ["GET","POST"])
+def home():
+    process_airtable_and_kml()
+    print(200)
+    return render_template("helloworld.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
